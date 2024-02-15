700e929999dfe4713e8c53b7efaca0a8aa165f62
Manish Pandey <79096969+manishpandeydev@users.noreply.github.com>
2024-02-15 16:47:31 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/",
+  base:"/Weather-App/",
   plugins: [react()],
   theme: jekyll-theme-THEME-NAME,
   
